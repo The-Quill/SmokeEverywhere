2290645055a8386d6c12cc20b3a3eb1d059bfcd6
rschrieken <rene.schrieken@gmail.com>
2016-03-31 00:37:40 +0200

    added Grave Yard as room target

--- ROChatTranscript.user.js
+++ ROChatTranscript.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Unstar/unpin/delete for room owners
 // @namespace    http://stackoverflow.com/users/578411/rene
-// @version      0.2
-// @description  RO Transcript 
+// @version      0.3
+// @description  RO Transcript
 // @author       rene
 // @match        *://chat.stackoverflow.com/transcript/*
 // @match        *://chat.stackexchange.com/transcript/*
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-/*global $:false, alert:false, MutationObserver:false */
+/*global $:false, alert:false, MutationObserver:false, input:false */
 
 (function () {
     'use strict';
@@ -27,65 +27,84 @@
             }
         });
     }
-    
-    function movetotrash(node) {
+
+    function moveto(node, target) {
         var fkey = document.getElementById('fkey').value,
             id = node.parentElement.id.split('-')[1],
-            target = 48058,
             href = $(node.parentElement).find('a'),
             re = /(?:\w|[.\/:])*\/transcript\/(\d+)/,
             matches;
+        if (target === undefined) {
+            target = input('move to room id');
+        }
         if (href.length > 0) {
             matches = re.exec($(href[0]).prop('href'));
             if (matches !== null && matches.length > 1) {
                 $.ajax({
                     type: 'POST',
                     data: 'ids=' + id + '&to=' + target + '&fkey=' + fkey,
-                    url: '/admin/movePosts/' + matches[1]
-                }).complete(function(){ $(node.parentElement).fadeOut(2000);  });
+                    url: '/admin/movePosts/' + matches[1],
+                    success: function () { $(node.parentElement).fadeOut(2000);  },
+                    error: function () { $(node.parentElement).css('background-color', 'yellow'); }
+                });
             }
         }
     }
 
-    function unpinBuilder(node) {
+    function movetotrash(node) {
+        moveto(node, 48058);
+    }
+
+    function movetoGraveYard(node) {
+        moveto(node, 90230);
+    }
+
+    function menuBuilder(node, labelText, spriteClass, callback) {
         var outer = document.createElement('span'),
             sprite = document.createElement('span'),
-            text = document.createTextNode("unpin/cancel stars (RO only)");
+            text = document.createTextNode(labelText);
 
-        sprite.className = "sprite sprite-ownerstar-off";
+        sprite.className = "sprite " + spriteClass;
         outer.className = "star";
+        outer.style.display = "block";
         outer.appendChild(sprite);
         outer.appendChild(text);
 
         outer.addEventListener(
             "click",
             function () {
-                unstar(node);
+                callback(node);
             },
             false
         );
         return outer;
     }
-    
-    function moveBuilder(node) {
-        var outer = document.createElement('span'),
-            sprite = document.createElement('span'),
-            text = document.createTextNode("move to trash (RO only)");
 
-        sprite.className = "sprite ";
-        outer.className = "";
-        outer.style.cursor = "pointer";
-        outer.appendChild(sprite);
-        outer.appendChild(text);
-
-        outer.addEventListener(
-            "click",
-            function () {
-                movetotrash(node);
-            },
-            false
+    function unpinBuilder(node) {
+        return menuBuilder(
+            node,
+            "unpin/cancel stars (RO only)",
+            "sprite-ownerstar-off",
+            unstar
         );
-        return outer;
+    }
+
+    function moveBuilder(node) {
+        return menuBuilder(
+            node,
+            "move to trash (RO only)",
+            "",
+            movetotrash
+        );
+    }
+
+    function graveyardBuilder(node) {
+        return menuBuilder(
+            node,
+            "move to Grave Yard (RO only)",
+            "",
+            movetoGraveYard
+        );
     }
 
     function addActions(node) {
@@ -103,6 +122,10 @@
                     );
                     node.insertBefore(
                         moveBuilder(node),
+                        currentNode
+                    );
+                    node.insertBefore(
+                        graveyardBuilder(node),
                         currentNode
                     );
                 }
